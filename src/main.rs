cc9ea1a6c2efde22d72236c060e4b82f9e45d491
Memo Chou <memochou1993@gmail.com>
2022-09-29 22:13:16 +0800

    Add main component

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    let count = use_state(|| 0);
+    let onclick = {
+        let count = count.clone();
+        move |_| {
+            count.set(*count + 1);
+        }
+    };
+
+    html! {
+        <div>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *count }</p>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
